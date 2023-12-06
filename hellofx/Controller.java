5c711d4662ad6cb7e0670d9d76462f86f8455969
Jonathan94453 <cino.jonathan@gmail.com>
2023-12-06 17:38:09 -0600

    Starting button functionality- got one button working

--- hellofx/Controller.java
+++ hellofx/Controller.java
@@ -2,16 +2,29 @@ package hellofx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color; 
+import javafx.scene.Group; 
 
 public class Controller{
     
 
     @FXML
     private Label label;
+    private Button startbutton;
+    
+public void handlebuttonpress(ActionEvent event) {
+    
+}
 
     public void initialize() {
        label.setText("Logan is gay");
-       info o = new info(label); 
-       o.returnlabel(); 
+       handlebuttonpress(startbutton); 
     }
 }
--- hellofx/Main.java
+++ hellofx/Main.java
@@ -4,14 +4,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color; 
+import javafx.scene.Group; 
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml")); 
+        Button clickstart = new Button(); 
+        clickstart.setText("Start the Program!");
+        clickstart.setTranslateX(350.0);
+        clickstart.setTranslateY(400.0);
         primaryStage.setTitle("Hello World");
-        Scene o = new Scene(root, 400, 300);  
-        primaryStage.setScene(o);
+        Group root2 = new Group(clickstart);
+        Scene scene2 = new Scene(root2, 800, 800, Color.BEIGE); 
+        Scene o = new Scene(root, 800, 800);  
+        primaryStage.setScene(scene2);
         primaryStage.show(); 
     }
 
