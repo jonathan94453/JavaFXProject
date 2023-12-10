b363b826853b233057f68299c1adcba0d580a742
Jonathan94453 <cino.jonathan@gmail.com>
2023-12-09 22:14:18 -0600

    save calculator

--- hellofx/Main.java
+++ hellofx/Main.java
@@ -5,30 +5,39 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
+import javafx.scene.Group;
 import javafx.scene.Scene; 
 
 
 public class Main extends Application {
-     public Scene currentscene; 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+  //Initialize the Scene               
+    double scenewidth = 1000; 
+    double sceneheight = 800; 
+    Group originroot = new Group();
+    Scene originscene = new Scene(originroot, scenewidth, sceneheight); 
+    Font calculatorfont = Font.loadFont(getClass().getResourceAsStream("/valuoldcaps.ttf"), 50);
+    Label welcome = new Label("Welcome to the Calculator"); 
+    welcome.setLayoutX(20);
+    welcome.setLayoutY(100);
+    welcome.setFont(calculatorfont);
+
+
+        
+//Root Children Adds 
+    originroot.getChildren().add(welcome);            
+               
                 
-                double scenewidth = 800; 
-                double sceneheight = 800; 
-                Label Titlelabel = new Label("Welcome to Westrichia"); 
-                Button Start = new Button(); 
-                environment2d background = new environment2d(scenewidth, sceneheight, Titlelabel, Start, primaryStage); 
-                setcurrentscene(background.getScene()); 
-primaryStage.getIcons().add(new Image("/Scene2.jpg")); 
-                    primaryStage.setTitle("Westrichia");
-                    primaryStage.setScene(currentscene);
-                    primaryStage.show(); 
+primaryStage.getIcons().add(new Image("icon.jpg")); 
+primaryStage.setTitle("Calculator");
+primaryStage.setScene(originscene);
+primaryStage.show(); 
     }
 
-public Scene setcurrentscene(Scene scene) {
-    currentscene = scene;
-    return currentscene; 
-}
+//Event Handlers 
 
     public static void main(String[] args) {
         launch(args); 
